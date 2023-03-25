dc4dfa128a9d1bf1115910340f0a490b743d68db
innocentdavid <paulinnocent04@gmail.com>
2023-03-25 20:02:55 +0100

    fgjmk

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cors())
 // }
 
 app.post('/api/notify', async (req, res) => {
-  const webhookUrl = 'https://hooks.slack.com/services/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'
+  const webhookUrl = 'https://hooks.slack.com/services/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'
   axios.post(webhookUrl, {
     text: req.body.message
   }).then(() => {
